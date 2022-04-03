fb47d578827e057861149fce3aeb66cdc08a96b8
refracta <58779799+refracta@users.noreply.github.com>
2022-04-03 23:11:00 +0900

    Add DefaultProcess test

---
+++ src/test/java/kr/ac/koreatech/os/test/process/DefaultProcessTest.java
@@ -0,0 +1,21 @@
+package kr.ac.koreatech.os.test.process;
+
+import kr.ac.koreatech.os.pss.process.impl.DefaultProcess;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+/**
+ * DefaultProcess 함수 테스트 클래스
+ *
+ * @author refracta
+ */
+public class DefaultProcessTest {
+    @Test
+    public void contiguousProcessesTest() {
+        DefaultProcess defaultProcess = new DefaultProcess(0, 0, 5);
+        List<DefaultProcess> processes = defaultProcess.getContiguousProcesses(3, 2);
+        System.out.println(processes.size());
+        System.out.println(processes);
+    }
+}
