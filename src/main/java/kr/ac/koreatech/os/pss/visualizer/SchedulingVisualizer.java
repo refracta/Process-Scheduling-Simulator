ebb6456c573974caf3271f88f6a5a8add563a08e
unta1337 <firstapple42@gmail.com>
2022-04-04 23:37:37 +0900

    시각화 레이아웃 데모 구현 중

--- src/main/java/kr/ac/koreatech/os/pss/visualizer/SchedulingVisualizer.java
+++ src/main/java/kr/ac/koreatech/os/pss/visualizer/SchedulingVisualizer.java
@@ -2,21 +2,12 @@ package kr.ac.koreatech.os.pss.visualizer;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class SchedulingVisualizer extends Application {
-    private Pane root;
-
-    private VBox leftMenu;
-
     /**
      * Starter.java start method
      *
@@ -28,14 +19,7 @@ public class SchedulingVisualizer extends Application {
         preLoad();
 
         Pane root = FXMLLoader.load(SchedulingVisualizer.class.getResource("SchedulingVisualizerMain.fxml"));
-        ((Text)((VBox)(root.getChildren().get(0))).getChildren().get(0)).setText("SEX");
-        ((VBox)(root.getChildren().get(0))).setAlignment(Pos.CENTER);
-        ((VBox)(root.getChildren().get(0))).setAlignment(Pos.CENTER);
-
         Scene scene = new Scene(root);
-
-        String styleSheet = SchedulingVisualizer.class.getResource("SchedulingVisualizerMain.css").toExternalForm();
-        scene.getStylesheets().add(styleSheet);
 
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
--- src/main/java/kr/ac/koreatech/os/pss/visualizer/SchedulingVisualizerController.java
+++ src/main/java/kr/ac/koreatech/os/pss/visualizer/SchedulingVisualizerController.java
@@ -1,15 +1,4 @@
 package kr.ac.koreatech.os.pss.visualizer;
 
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
-import javafx.scene.text.Text;
-
-import java.io.IOException;
-
 public class SchedulingVisualizerController {
-    @FXML
-    Text test;
 }
