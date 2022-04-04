dbdf60b7b80ced031e768bc4b012152e00fd6e47
refracta <58779799+refracta@users.noreply.github.com>
2022-04-05 07:13:21 +0900

    Remove getContextClassLoader & Add method comments

--- src/main/java/kr/ac/koreatech/os/pss/utility/ResourceUtils.java
+++ src/main/java/kr/ac/koreatech/os/pss/utility/ResourceUtils.java
@@ -7,7 +7,19 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 리소스 로딩에 필요한 유틸리티 클래스
+ *
+ * @author refracta
+ */
 public class ResourceUtils {
+    /**
+     * 주어진 경로 폴더에 있는 리소스 요소의 이름을 리스트로 가져온다.
+     *
+     * @param path 리소스 경로 폴더
+     * @return 리소스 요소 리스트
+     * @throws IOException
+     */
     public static List<String> getResourceFiles(String path) throws IOException {
         List<String> filenames = new ArrayList<>();
 
@@ -23,15 +35,23 @@ public class ResourceUtils {
         return filenames;
     }
 
+    /**
+     * 주어진 경로의 리소스를 스트림으로 가져온다.
+     *
+     * @param resource 리소스 경로
+     * @return 리소스의 InputStream
+     */
     public static InputStream getResourceAsStream(String resource) {
-        InputStream in = getContextClassLoader().getResourceAsStream(resource);
+        InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream(resource);
         return in == null ? ResourceUtils.class.getResourceAsStream(resource) : in;
     }
 
-    private static ClassLoader getContextClassLoader() {
-        return Thread.currentThread().getContextClassLoader();
-    }
-
+    /**
+     * 주어진 클래스의 패키지 경로를 반환한다.
+     *
+     * @param target
+     * @return 클래스의 패키지 경로 ('/'로 구분됨)
+     */
     public static String getPackagePath(Class target) {
         return target.getPackage().getName().replaceAll("\\.", "/") + "/";
     }
