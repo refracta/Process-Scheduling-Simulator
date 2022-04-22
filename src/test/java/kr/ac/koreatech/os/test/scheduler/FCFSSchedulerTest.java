b38ec9b5e3517a38edb39fe138880d6b76d8ec63
refracta <58779799+refracta@users.noreply.github.com>
2022-04-22 18:31:04 +0900

    Refactoring: to simpler

--- src/test/java/kr/ac/koreatech/os/test/scheduler/FCFSSchedulerTest.java
+++ src/test/java/kr/ac/koreatech/os/test/scheduler/FCFSSchedulerTest.java
@@ -23,23 +23,21 @@ public class FCFSSchedulerTest {
         FCFSScheduler fcfsScheduler = new FCFSScheduler();
         AbstractCore[] cores = {new EfficiencyCore(), new PerformanceCore(), new PerformanceCore()};
         DefaultProcess[] processes = {
-                new DefaultProcess(0, 1, 4),
-                new DefaultProcess(1, 2, 4),
-                new DefaultProcess(2, 3, 7),
-                new DefaultProcess(3, 3, 7),
-                new DefaultProcess(4, 3, 7),
-                new DefaultProcess(5, 3, 7),
+                new DefaultProcess(1, 4),
+                new DefaultProcess(2, 4),
+                new DefaultProcess(3, 7),
+                new DefaultProcess(3, 7),
+                new DefaultProcess(3, 7),
+                new DefaultProcess(3, 7),
         };
         ScheduleData scheduleData = fcfsScheduler.schedule(cores, processes);
-        double totalPowerUsage = 0;
         for (int i = 0; i < cores.length; i++) {
             AbstractCore currentCore = cores[i];
             List<DefaultProcess> coreSchedule = scheduleData.getSchedule().get(currentCore);
             double powerUsage = scheduleData.getPowerUsage(currentCore);
-            totalPowerUsage += powerUsage;
             System.out.println("Core[" + i + ":" + currentCore.getClass().getSimpleName() + "]" + ": " + "[" + coreSchedule.stream().map(AbstractProcess::getName).collect(Collectors.joining(", ")) + "]: " + powerUsage + "W");
         }
-        System.out.println("Total power usage: " + totalPowerUsage + "W");
+        System.out.println("Total power usage: " + scheduleData.getTotalPowerUsage() + "W");
         System.out.println("Result processes:");
         System.out.println(scheduleData.getResultProcesses().stream().map(p -> "\t" + p.toString()).collect(Collectors.joining("\n")));
         System.out.println("Average response time: " + scheduleData.getAverageResponseTime());
