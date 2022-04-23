b99038aeb6feda130734fce9faec70fdc6dab15b
refracta <58779799+refracta@users.noreply.github.com>
2022-04-23 19:27:04 +0900

    Fix inner structure (using inheritance)

--- src/main/java/kr/ac/koreatech/os/pss/scheduler/impl/FCFSScheduler.java
+++ src/main/java/kr/ac/koreatech/os/pss/scheduler/impl/FCFSScheduler.java
@@ -1,43 +1,17 @@
 package kr.ac.koreatech.os.pss.scheduler.impl;
 
-import kr.ac.koreatech.os.pss.core.AbstractCore;
 import kr.ac.koreatech.os.pss.process.impl.DefaultProcess;
-import kr.ac.koreatech.os.pss.scheduler.AbstractScheduler;
-import kr.ac.koreatech.os.pss.scheduler.data.ScheduleData;
+import kr.ac.koreatech.os.pss.scheduler.PriorityScheduler;
 
 import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.PriorityQueue;
 
 /**
- * First Come First Service 스케쥴러 클래스
+ * First Come First Service 스케줄러 클래스
  *
  * @author refracta
  */
-public class FCFSScheduler extends AbstractScheduler {
-
-    @Override
-    protected void schedule(int time, List<AbstractCore> cores, List<DefaultProcess> processes, ScheduleData scheduleData) {
-        Map<AbstractCore, List<DefaultProcess>> schedule = scheduleData.getSchedule();
-        PriorityQueue<DefaultProcess> processQueue = new PriorityQueue<>(Comparator.comparingInt(DefaultProcess::getArrivalTime));
-        processQueue.addAll(processes.stream().filter(p -> !p.isFinished() && p.getArrivalTime() <= time).toList());
-        for (AbstractCore core : cores) {
-            List<DefaultProcess> coreSchedule = schedule.get(core);
-            if (processQueue.isEmpty()) {
-                break;
-            }
-            if (!coreSchedule.isEmpty()) {
-                if (coreSchedule.size() < time + 1) {
-                    // DefaultProcess previous = coreSchedule.get(time - 1);
-                    DefaultProcess targetProcess = processQueue.poll();
-                    coreSchedule.addAll(targetProcess.getContiguousProcesses(core.getPerformance()));
-                } // else -> already exist schedule
-            } else {
-                DefaultProcess targetProcess = processQueue.poll();
-                coreSchedule.addAll(targetProcess.getContiguousProcesses(core.getPerformance()));
-            }
-        }
-
+public class FCFSScheduler extends PriorityScheduler {
+    public FCFSScheduler() {
+        super(Comparator.comparingInt(DefaultProcess::getArrivalTime));
     }
 }
