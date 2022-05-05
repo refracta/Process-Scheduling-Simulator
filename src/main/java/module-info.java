3d7293d22f7fce84b52a78ccb0dc9d09b062e195
refracta <58779799+refracta@users.noreply.github.com>
2022-05-06 06:32:10 +0900

    Fix issue #21

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,11 +8,12 @@ module kr.ac.koreatech.os.pss {
     requires java.logging;
     requires com.jfoenix;
     requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.fontawesome5;
 
-    opens kr.ac.koreatech.os.pss.app to javafx.graphics, javafx.fxml, com.jfoenix, org.kordamp.ikonli.javafx;
-    opens kr.ac.koreatech.os.pss.app.legacy to com.jfoenix, javafx.fxml, javafx.graphics, org.kordamp.ikonli.javafx;
-    opens kr.ac.koreatech.os.pss.app.loader to com.jfoenix, javafx.fxml, javafx.graphics, org.kordamp.ikonli.javafx;
-    opens kr.ac.koreatech.os.pss.app.component.pane to com.jfoenix, javafx.fxml, javafx.graphics, org.kordamp.ikonli.javafx;
-    opens kr.ac.koreatech.os.pss.app.loader.annotation to com.jfoenix, javafx.fxml, javafx.graphics, org.kordamp.ikonli.javafx;
-    opens kr.ac.koreatech.os.pss.app.loader.utils to com.jfoenix, javafx.fxml, javafx.graphics, org.kordamp.ikonli.javafx;
+    opens kr.ac.koreatech.os.pss.app to javafx.graphics, javafx.fxml, com.jfoenix, org.kordamp.ikonli.javafx, org.kordamp.ikonli.fontawesome5;
+    opens kr.ac.koreatech.os.pss.app.legacy to com.jfoenix, javafx.fxml, javafx.graphics, org.kordamp.ikonli.javafx, org.kordamp.ikonli.fontawesome5;
+    opens kr.ac.koreatech.os.pss.app.loader to com.jfoenix, javafx.fxml, javafx.graphics, org.kordamp.ikonli.javafx, org.kordamp.ikonli.fontawesome5;
+    opens kr.ac.koreatech.os.pss.app.component.pane to com.jfoenix, javafx.fxml, javafx.graphics, org.kordamp.ikonli.javafx, org.kordamp.ikonli.fontawesome5;
+    opens kr.ac.koreatech.os.pss.app.loader.annotation to com.jfoenix, javafx.fxml, javafx.graphics, org.kordamp.ikonli.javafx, org.kordamp.ikonli.fontawesome5;
+    opens kr.ac.koreatech.os.pss.app.loader.utils to com.jfoenix, javafx.fxml, javafx.graphics, org.kordamp.ikonli.javafx, org.kordamp.ikonli.fontawesome5;
 }
