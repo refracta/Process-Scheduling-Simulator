2dbced880dea7f8a02877214c64b9ecc2f270c37
refracta <58779799+refracta@users.noreply.github.com>
2022-05-05 19:38:07 +0900

    Merge manually (feature/basic-structure-visual-demo)

---
+++ src/main/java/kr/ac/koreatech/os/pss/app/legacy/LSchedulingVisualizer.java
@@ -0,0 +1,47 @@
+package kr.ac.koreatech.os.pss.app.legacy;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+/**
+ * 스케쥴러 비주얼라이저 클래스
+ *
+ * @author unta1337
+ * @author refracta
+ */
+public class LSchedulingVisualizer extends Application {
+
+    /**
+     * 비주얼라이저 Java-FX 엔트리 포인트
+     *
+     * @param primaryStage Stage
+     * @throws Exception Application start's Exception
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        preLoad();
+
+        LSchedulerControls schedulerControls = LSchedulerControls.getSchedulerControls();
+
+        Scene scene = new Scene(schedulerControls.getRoot());
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    /**
+     * 외부 폰트 목록
+     */
+    private static final String[] FONTS = {"NanumSquareB.ttf", "NanumSquareEB.ttf", "NanumSquareL.ttf", "NanumSquareR.ttf", "NanumSquare_acB.ttf", "NanumSquare_acEB.ttf", "NanumSquare_acL.ttf", "NanumSquare_acR.ttf"};
+
+    /**
+     * 폰트 및 이미지 등 리소스를 불러옴
+     */
+    public void preLoad() {
+        for (String font : FONTS) {
+            Font.loadFont(LSchedulingVisualizer.class.getResourceAsStream("font" + "/" + font), -1);
+        }
+    }
+}
---
+++ src/main/java/kr/ac/koreatech/os/pss/app/legacy/timeline/impl/LProcessTimeLine.java
@@ -0,0 +1,126 @@
+package kr.ac.koreatech.os.pss.app.legacy.timeline.impl;
+
+import javafx.scene.Cursor;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Line;
+import kr.ac.koreatech.os.pss.app.legacy.LProcessControls;
+import kr.ac.koreatech.os.pss.app.legacy.timeline.LAbstractTimeLine;
+import kr.ac.koreatech.os.pss.process.impl.DefaultProcess;
+
+import java.io.IOException;
+
+public class LProcessTimeLine extends LAbstractTimeLine {
+    private DefaultProcess process;
+    private LTimeLineBar timeLineBar;
+
+    public LProcessTimeLine(int criteriaEndTime, double width, double height, LProcessControls processControls) throws IOException {
+        super(width, height, processControls);
+
+        this.setOnMouseMoved(this::onMouseMoved);
+        this.setOnMouseExited(this::onMouseExited);
+        this.setOnMouseDragged(this::onMouseDragged);
+        this.setOnMouseReleased(this::onMouseReleased);
+
+        this.process = new DefaultProcess(0, 1);
+        this.timeLineBar = new LTimeLineBar(process.getArrivalTime(), process.getBurstTime(), width / criteriaEndTime, height);
+
+        updateScale(criteriaEndTime);
+    }
+
+    @Override
+    public void updateScale(int criteriaEndTime) {
+        try {
+            getChildren().remove(0, getChildren().size());
+        } catch (Exception exception) { }
+        for (int i = 0; i <= criteriaEndTime; i++) { this.getChildren().add(new Line(i * processControls.getLengthFactor(), 0, i * processControls.getLengthFactor(), 30)); }
+        this.getChildren().add(this.timeLineBar);
+
+        timeLineBar.update(timeLineBar.getArrivalTime(), timeLineBar.getBurstTime(), processControls.getLengthFactor());
+        timeLineBar.setLayoutX(timeLineBar.getArrivalTime() * processControls.getLengthFactor());
+    }
+
+    public void onMouseMoved(MouseEvent event) {
+        double processedX = event.getX() - timeLineBar.getLayoutX();
+
+        boolean leftCondition = timeLineBar.isInLeft(processedX);
+        boolean rightCondition = timeLineBar.isInRight(processedX, processControls.getLengthFactor());
+        boolean middleCondition = timeLineBar.isInMiddle(processedX, processControls.getLengthFactor());
+
+        if (leftCondition || rightCondition || middleCondition) {
+            if (leftCondition || rightCondition)
+                setCursor(Cursor.H_RESIZE);
+            else
+                setCursor(Cursor.DEFAULT);
+            timeLineBar.makeTransparent();
+        } else {
+            timeLineBar.makeOpaque();
+            setCursor(Cursor.DEFAULT);
+        }
+    }
+
+    public void onMouseExited(MouseEvent event) {
+        timeLineBar.makeOpaque();
+        setCursor(Cursor.DEFAULT);
+    }
+
+    public void onMouseDragged(MouseEvent event) {
+        double processedX = event.getX() - timeLineBar.getLayoutX();
+
+        if (actionState == ActionState.IDLE) {
+            if (timeLineBar.isInMiddle(processedX, processControls.getLengthFactor()))
+                actionState = ActionState.MOVE;
+            else if (timeLineBar.isInLeft(processedX))
+                actionState = ActionState.EXTEND_LEFT;
+            else if (timeLineBar.isInRight(processedX, processControls.getLengthFactor()))
+                actionState = ActionState.EXTEND_RIGHT;
+        }
+
+        switch (actionState) {
+            case EXTEND_LEFT:
+                setCursor(Cursor.H_RESIZE);
+                double newWidth = timeLineBar.getLayoutX() + timeLineBar.getWidth() - Math.max(0, event.getX());
+                timeLineBar.setLayoutX(Math.max(0, event.getX()));
+                timeLineBar.setWidth(newWidth);
+                break;
+            case EXTEND_RIGHT:
+                setCursor(Cursor.H_RESIZE);
+                newWidth = event.getX() - timeLineBar.getLayoutX();
+                timeLineBar.setWidth(newWidth);
+                break;
+            case MOVE:
+                timeLineBar.setLayoutX(Math.max(0, event.getX() - timeLineBar.getWidth() / 2));
+                break;
+        }
+    }
+
+    public void onMouseReleased(MouseEvent event) {
+        switch (actionState) {
+            case EXTEND_LEFT:
+                int index = timeLineBar.getLeftExpendedIndex(event.getX(), processControls.getLengthFactor());
+                timeLineBar.update(index, timeLineBar.getEndTime() - index, processControls.getLengthFactor());
+                timeLineBar.setLayoutX(timeLineBar.getArrivalTime() * processControls.getLengthFactor());
+                break;
+            case EXTEND_RIGHT:
+                index = timeLineBar.getRightExpendedIndex(event.getX(), processControls.getLengthFactor());
+                timeLineBar.update(timeLineBar.getArrivalTime(), index - timeLineBar.getArrivalTime(), processControls.getLengthFactor());
+                break;
+            case MOVE:
+                index = timeLineBar.getMovedIndex(event.getX() - timeLineBar.getWidth() / 2, processControls.getLengthFactor());
+                timeLineBar.update(index, timeLineBar.getBurstTime(), processControls.getLengthFactor());
+                timeLineBar.setLayoutX(timeLineBar.getArrivalTime() * processControls.getLengthFactor());
+                break;
+        }
+
+        process.setArrivalTime(timeLineBar.getArrivalTime());
+        process.setBurstTime(timeLineBar.getBurstTime());
+
+        setCursor(Cursor.DEFAULT);
+        actionState = ActionState.IDLE;
+
+        processControls.updateAllScales();
+    }
+
+    public DefaultProcess getProcess() {
+        return process;
+    }
+}
