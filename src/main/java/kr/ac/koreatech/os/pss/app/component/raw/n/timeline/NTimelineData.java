100cd7551da246ce5f744a1caea3cb6750e908c6
refracta <58779799+refracta@users.noreply.github.com>
2022-05-08 19:09:21 +0900

    Fail

--- src/main/java/kr/ac/koreatech/os/pss/app/component/raw/n/timeline/NTimelineData.java
+++ src/main/java/kr/ac/koreatech/os/pss/app/component/raw/n/timeline/NTimelineData.java
@@ -1,4 +1,11 @@
 package kr.ac.koreatech.os.pss.app.component.raw.n.timeline;
 
 public record NTimelineData(int value, double renderX) {
+    @Override
+    public String toString() {
+        return "NTimelineData{" +
+                "value=" + value +
+                ", renderX=" + renderX +
+                '}';
+    }
 }
